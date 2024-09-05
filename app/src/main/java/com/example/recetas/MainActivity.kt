6a6ca46abc81544371927ba4f662ae10fa7ae14f
PablorellanaM <ore20555@uvg.edu.gt>
2024-09-04 20:25:13 -0600

    Update

--- app/src/main/java/com/example/recetas/MainActivity.kt
+++ app/src/main/java/com/example/recetas/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.recetas
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.material.Text
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
-import androidx.appcompat.app.ActionBarDrawerToggle
-import android.view.GravityCompat
-import android.content.Intent
+import com.example.recetas.PopularRecipesActivity
 
 
 class MainActivity : AppCompatActivity() {
@@ -52,5 +59,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     RecetasTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun RecetasTheme(content: @Composable () -> Unit) {
+    MaterialTheme {
+        content()
     }
 }
---
+++ app/src/main/java/com/example/recetas/PopularRecipesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.recetas
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PopularRecipesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_popular_recipes) // Asegúrate de que este archivo exista
+    }
+}
